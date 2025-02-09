0880d6ce0ac35e12a58598fa5c0e3b668f7e85f6
RiaJ <koselevaviktoria12@gmail.com>
2025-02-09 16:43:13 +0300

    First dark disign

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,52 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import styles from './App.module.scss'
+import Strawberry from './components/Strawberry/Strawberry'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <header>
+        <div class={styles.logo}><h1>ЛОГО</h1></div>
+        <nav class={styles.nav_header}>
+          <div class={`${styles.nav_link}`}><h1>О нас</h1></div>
+          <div class={`${styles.nav_link}`}><h1>Контакты</h1></div>
+          <div class={`${styles.nav_link}`}><h1>Каталог</h1></div>
+          <div class={`${styles.nav_link}`}><h1>Заказать</h1></div>
+        </nav>
+        <div class={styles.header_btn_order}> <h1>Заказать</h1></div>
+      </header>
+      <div class={styles.main}>
+      <main > 
+       <div class={styles.main_describtion} >
+          <div class={styles.main_header}><h1>ЭТО НАША КОМПАНИЯ</h1></div>
+          <div class={styles.main_text}><p>Добро пожаловать в [Название компании] – место, где свежесть природы встречается с роскошью шоколада. Мы создаем изысканные десерты, превращая сочную отборную клубнику в настоящее произведение кулинарного искусства.</p></div>
+          <div class={styles.button}><h1>Заказать</h1></div>
+       </div>
+      <img src="src\images\1.png"></img>
+      </main>
+      <div class={styles.catalagy_name}>       
+        <h1 class={styles.h1_catalogy}>КАТАЛОГ</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div class={styles.frame_catalogy}>
+        <div class={styles.catalogy}>
+          <div class={styles.card}></div>
+          <div class={styles.card}></div>
+          <div class={styles.card}></div>
+          <div class={styles.card}></div>
+          <div class={styles.card}></div>
+          <div class={styles.card}></div>
+          <div class={styles.card}></div>
+          <div class={styles.card}></div>
+          <div class={styles.card}></div>
+
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </div>
+      <div class={styles.straw_img}>
+      </div>
+      
     </>
   )
 }
